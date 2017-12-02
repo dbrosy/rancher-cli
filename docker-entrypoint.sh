df3946c6ddb1c9edaeb589098910652485b05eb5
dbrosy <dbrosy@hotmail.com>
2017-12-02 21:48:55 +1100

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mkdir -p /home/docker-user/.rancher
+
+[ -f /home/docker-user/.rancher/cli.json ] && echo "Found /home/docker-user/.rancher/cli.json" || echo "{\"accessKey\":\"$ACCESS_KEY\",\"secretKey\":\"$SECRET_KEY\",\"url\":\"$URL\",\"environment\":\"$ENV\"}" > /home/docker-user/.rancher/cli.json
+exec "$@"
